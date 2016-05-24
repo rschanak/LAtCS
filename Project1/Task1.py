4206f42b379eea176604ebd37aff8b3863b21dd6
rschanak <rschanak@gmail.com>
2016-05-24 12:22:42 -0700

    Fixed Task 1

--- Project1/Task1.py
+++ Project1/Task1.py
@@ -4,4 +4,4 @@ minutes_in_hour = 60
 minutes_in_day = 24 * minutes_in_hour
 minutes_in_week = minutes_in_day * 7
 
-print("The number of minutes in an hour is: " + minutes_in_week)
+print("The number of minutes in a week is: " + str(minutes_in_week))
